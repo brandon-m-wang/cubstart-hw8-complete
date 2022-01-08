0109e90317208c7c783e76a7330e496ec1bac66c
brandon-m-wang <brandonwang0503@berkeley.edu>
2022-01-08 16:13:25 -0600

    quickfix

--- react-client/src/components/closeFriend/CloseFriend.jsx
+++ react-client/src/components/closeFriend/CloseFriend.jsx
@@ -1,9 +1,16 @@
 import "./closeFriend.css";
 
-export default function CloseFriend({user}) {
+export default function CloseFriend({ user }) {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     <li className="sidebarFriend">
-      <img className="sidebarFriendImg" src={user.profilePicture} alt="" />
+      <img
+        className="sidebarFriendImg"
+        src={
+          user.profilePicture ? user.profilePicture : PF + "person/noAvatar.png"
+        }
+        alt=""
+      />
       <span className="sidebarFriendName">{user.username}</span>
     </li>
   );
